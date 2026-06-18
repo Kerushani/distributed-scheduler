74e65bab65171cad01ba3b7aba74dd76b6b8ca3b
Kerushani Sivaneswaran <99515111+Kerushani@users.noreply.github.com>
2026-06-17 23:37:10 -0400

    add engine

--- simulator/engine.go
+++ simulator/engine.go
@@ -1,3 +1,12 @@
+pakage sim
+
+import (
+	"scheduler-sim/cluster"
+	"scheduler-sim/jobs"
+	"scheduler-sim/scheduler"
+	"scheduler-sim/metrics"
+)
+
 type Engine struct {
 	Cluster *cluster.Cluster
 	Jobs []*jobs.Jobs
@@ -10,22 +19,54 @@ type Engine struct {
 
 	PendingJobs []*jobs.Job
 	RunningJobs map[string]*jobs.Job
+	CompletedJobs []*jobs.Job
+}
+
+type Decision struct {
+	JobID string
+	NodeID string
+}
+
+func (e *Enginer) applyDecisions(decisions []Decision){
+	for _, d := range decisions{
+		job := e.findJob(d.JobID)
+		node := e.Cluster.Nodes[d.NodeID]
+
+		if node.CanFit(job){
+			node.Assign(job, e.Tick)
+
+			e.removePending(job.ID)
+		}
+	}
+}
+
+func (e *Engine) executeTick() {
+	for _, node := range e.Cluster.Nodes{
+		node.Tick()
+
+		for _, job := range node.RunningJobs{
+			if job.Duration <= 0 {
+				e.CompletedJobs = append(e.CompletedJobs, job)
+			}
+		}
+	}
+}
+
+func (e *Engine) loadJobs() {
+	//no-op for v1 since no batch mode
 }
 
 func (e *Engine) Run(maxTicks int) {
 	for e.Tick < maxTicks{
-		e.injectJobs()
+		e.loadJobs()
+		decisions := e.Scheduler.Schedule(e.Cluster, e.PendingJobs, e.Tick)
+		e.applyDecisions(decisions)
+		e.ExecuteTick()
+		e.Metrics.Collect(e.Cluster, e.PendingJobs, e.CompletedJobs, e.Tick)
 
-		e.Scheduler.Schedule(e.Cluster, e.PendingJobs, e.Tick)
-
-		e.executeTick()
-
-		e.Metrics.Collect(e.Cluster, e.RunningJobs, e.Tick)
-
-		e.Tick++
-
-		if e.allJobsCompleted(){
+		if len(e.PendingJobs) == 0 && e.allNodesIdle(){
 			break
 		}
+		e.Tick++
 	}
 }
